65cb9bace8e31dd66c4d612ab049be4fb534d883
brumenalkid-design <brumenalkid@gmail.com>
2026-01-12 17:34:48 -0300

    Atualizar o web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Config;
+
+// FORCE JWT SECRET IN MEMORY TO KILL THE "SECRET NOT DEFINED" ERROR
+Config::set('jwt.secret', 'OTY4N2Y1ZTM0YjI5ZDVhZDVmOTU1ZTM2ZDU4NTQ=');
+Config::set('app.cipher', 'AES-256-CBC');
 
 Route::get('/', function () {
-    return "<h1>SISTEMA ONLINE</h1><p>Se voce esta vendo isso, as chaves foram aceitas. Agora remova este bloco do web.php para carregar o jogo.</p>";
+    return "<h1>SYSTEM ONLINE</h1><p>Environment keys forced. Database deployment in progress.</p>";
 });
 
+// ORIGINAL ROUTES
 include_once(__DIR__ . '/groups/layouts/app.php');
 
