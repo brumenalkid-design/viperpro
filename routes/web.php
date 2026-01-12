4311114fd2452e9e3d4713f980afd256da28823e
brumenalkid-design <brumenalkid@gmail.com>
2026-01-12 16:40:10 -0300

    Atualizar o web.php

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,10 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
-// Rota inicial simples para testar se o site abriu
 Route::get('/', function () {
-    return view('welcome'); // Ou a rota principal do seu jogo
+    return "Servidor Online - Chaves Geradas";
 });
 
-// Suas rotas originais
 include_once(__DIR__ . '/groups/layouts/app.php');
 
 
