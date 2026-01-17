24e213185609259998209098ea4f4b96ae6227a8
brumenalkid-design <brumenalkid@gmail.com>
2026-01-17 18:54:38 -0300

    Atualizar o index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
