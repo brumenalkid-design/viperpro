8be11faeba74aefca429b67b94b6302b4408e2b4
brumenalkid-design <brumenalkid@gmail.com>
2026-01-19 19:37:16 -0300

    Atualizar o web.php

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Config;
 
-// HARDCODED OVERRIDE - FORÇA AS CHAVES NA MEMÓRIA
-config(['app.key' => 'base64:OTY4N2Y1ZTM0YjI5ZDVhZDVmOTU1ZTM2ZDU4NTQ=']);
-config(['app.cipher' => 'AES-256-CBC']);
-config(['jwt.secret' => 'OTY4N2Y1ZTM0YjI5ZDVhZDVmOTU1ZTM2ZDU4NTQ=']);
-
 Route::get('/', function () {
     return "<h1>SYSTEM ONLINE</h1><p>The core application is now operational.</p>";
 });
