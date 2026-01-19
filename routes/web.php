d2895d345c897d753fca66c05347b34fd1db3514
brumenalkid-design <brumenalkid@gmail.com>
2026-01-19 19:58:13 -0300

    Atualizar o web.php

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Config;
 
-// O bloco "SYSTEM ONLINE" foi removido daqui para permitir o carregamento das rotas reais.
-
 // CARREGA AS ROTAS DO SISTEMA
 if (file_exists(__DIR__ . '/groups/layouts/app.php')) {
     include_once(__DIR__ . '/groups/layouts/app.php');
